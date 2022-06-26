2169160c40dd4cee6055cfc98f35cdf4392f093d
Sabuandri <sababeridzes@gmail.com>
2022-06-26 19:29:42 +0400

    New Saved GitHub Test

--- Lesson1.py
+++ Lesson1.py
@@ -1,2 +1,48 @@
-print("hello")
-print("work for me")
+from pygame import *
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+
+        if keys[K_RIGHT] and self.rect.x < 700 - 80:
+            self.rect.x += self.speed
+        if keys[K_DOWN] and self.rect.x < 700 - 80:
+            self.rect.y += self.speed
+
+    def fire(self):
+        bullet = Bullet('bullet.png', self.rect.centerx, self.rect.top, -10)
+        bullets.add(bullet)
+
+clock = time.Clock()
+
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                if num_fire < 5 and not reload_time:
+                    alien.fire()
+                    num_fire += 1
+                if not reload_time and num_fire >= 5:
+                    last_time = dro()
+                    reload_time = True
+
+
+display.update()
+clock.tick(60)
